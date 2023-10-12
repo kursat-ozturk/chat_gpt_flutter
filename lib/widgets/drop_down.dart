224da210d5f1d0a736b81110a459ee5db60726f5
Kürşat Öztürk <kursatsoftware@gmail.com>
2023-10-12 16:24:51 +0300

    displayed the all models using FutureBuilder

--- lib/widgets/drop_down.dart
+++ lib/widgets/drop_down.dart
@@ -1,4 +1,7 @@
 import 'package:chat_gpt_flutter/constants/constants.dart';
+import 'package:chat_gpt_flutter/models/models_model.dart';
+import 'package:chat_gpt_flutter/services/api_service.dart';
+import 'package:chat_gpt_flutter/widgets/text_widget.dart';
 import 'package:flutter/material.dart';
 
 class ModelsDropDownWidget extends StatefulWidget {
@@ -9,19 +12,44 @@ class ModelsDropDownWidget extends StatefulWidget {
 }
 
 class _ModelsDropDownWidgetState extends State<ModelsDropDownWidget> {
-  String currentModels = 'Model1';
+  String currentModels = 'text-davinci-003';
 
   @override
   Widget build(BuildContext context) {
-    return DropdownButton(
-      dropdownColor: scaffoldBackgroundColor,
-      iconEnabledColor: Colors.white,
-      items: getModelsItem,
-      value: currentModels,
-      onChanged: (value) {
-        setState(() {
-          currentModels = value.toString();
-        });
+    return FutureBuilder<List<ModelsModel>>(
+      future: ApiService.getModels(),
+      builder: (context, snapshot) {
+        if (snapshot.hasError) {
+          return Center(
+            child: TextWidget(
+              label: snapshot.error.toString(),
+            ),
+          );
+        }
+        return snapshot.data == null || snapshot.data!.isEmpty
+            ? const SizedBox.shrink()
+            : FittedBox(
+              child: DropdownButton(
+                  dropdownColor: scaffoldBackgroundColor,
+                  iconEnabledColor: Colors.white,
+                  items: List<DropdownMenuItem<String>>.generate(
+                    snapshot.data!.length,
+                    (index) => DropdownMenuItem(
+                      value: snapshot.data![index].id,
+                      child: TextWidget(
+                        label: snapshot.data![index].id,
+                        fontSize: 15,
+                      ),
+                    ),
+                  ),
+                  value: currentModels,
+                  onChanged: (value) {
+                    setState(() {
+                      currentModels = value.toString();
+                    });
+                  },
+                ),
+            );
       },
     );
   }
