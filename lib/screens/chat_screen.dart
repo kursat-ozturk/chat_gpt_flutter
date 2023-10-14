f8062dc60a8f6f8b715d3802da1a95dc0c3db270
Kürşat Öztürk <kursatsoftware@gmail.com>
2023-10-14 16:01:47 +0300

    get a better response, and avoid spamming

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -144,6 +144,17 @@ class _ChatScreenState extends State<ChatScreen> {
   Future<void> sendMessageFCT(
       {required ModelsProvider modelsProvider,
       required ChatProvider chatProvider}) async {
+        if (_isTyping) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: TextWidget(
+            label: "You can not send multiple messages at a time.",
+          ),
+          backgroundColor: Colors.red,
+        ),
+      );
+      return;
+    }
     if (textEditingController.text.isEmpty) {
       ScaffoldMessenger.of(context).showSnackBar(
         const SnackBar(
@@ -156,15 +167,16 @@ class _ChatScreenState extends State<ChatScreen> {
       return;
     }
     try {
+      String msg = textEditingController.text;
       setState(() {
         _isTyping = true;
         //chatList.add(ChatModel(msg: textEditingController.text, chatIndex: 0));
-        chatProvider.addUserMessage(msg: textEditingController.text);
+        chatProvider.addUserMessage(msg: msg);
         textEditingController.clear();
         focusNode.unfocus();
       });
       await chatProvider.sendMessageAndGetAnswers(
-          msg: textEditingController.text,
+          msg: msg,
           chosenModelId: modelsProvider.getCurrentModel);
       // chatList.addAll(await ApiService.sendMessage(
       //   message: textEditingController.text,
